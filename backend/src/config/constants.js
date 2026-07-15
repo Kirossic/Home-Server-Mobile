42d3edc05116ad394332e8672400b8863ebdcf51
Maxim <183989888+Kirossic@users.noreply.github.com>
2026-07-15 15:39:46 +0300

    добавлены конфигурационные константы

---
+++ backend/src/config/constants.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const os = require('os');
+
+const START_DIR = os.homedir();
+const HOME_DIR = path.join(START_DIR, 'projects');
+
+module.exports = {
+    PORT: 8080,
+    HOST: '0.0.0.0',
+    IDE_PORT: 8085,
+    START_DIR,
+    BASHRC_PATH: path.join(START_DIR, '.bashrc'),
+    LINKS_PATH: path.resolve(__dirname, '../../links.json'),
+    PROJECTS_DIR: HOME_DIR,
+    PG_DIR: path.join(START_DIR, 'postgresql'),
+    PROJECTS_DIR_NAME: 'projects',
+    HOME_DIR,
+};
