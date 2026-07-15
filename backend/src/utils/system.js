9101e457d13fb428948fe505107a5f3fcc1515d9
Maxim <183989888+Kirossic@users.noreply.github.com>
2026-07-15 16:27:10 +0300

    Выносим в отдельный файл модули отвечающие за получение системных данных

---
+++ backend/src/utils/system.js
@@ -0,0 +1,26 @@
+const os = require('os');
+const { execCommand } = require('./exec');
+const { stdout } = require('process');
+
+function getDiskSpace() {
+    return execCommand('df -h /data 2>/dev/null').then(({stdout}) => {
+        if (!stdout) return {total: '-', used: '-'};
+        const lines = stdout.trim().split('\n');
+        if (lines.length < 2) return {total: '-', used: '-'};
+        const parts = lines[1].replace(/\s+/g, ' ').split(' ')
+        return {total: parts[1] || '-', used: parts[2] || '-' };
+    });
+}
+
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+
+    for (const name in interfaces) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) return iface.address;
+        }
+    }
+    return '127.0.0.1';
+}
+
+module.exports = { getDiskSpace, getLocalIP };
